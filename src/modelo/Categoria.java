5a0de41e6d041b63e62097404a48b332687ad197
Sebastian <sebasbteancourt14@gmail.com>
2025-02-22 18:19:48 -0500

    Añadido Singleton, Modificado clases: Categoria, Producto

--- src/modelo/Categoria.java
+++ src/modelo/Categoria.java
@@ -4,10 +4,54 @@
  */
 package modelo;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author MI PC
  */
 public class Categoria {
+    String nombre, descripcion;
+    int Id;
+    LocalDate fechaCreacion;
+
+    public Categoria(String nombre, int Id) {
+        this.nombre = nombre;
+        this.Id = Id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public LocalDate getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDate fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+    
+    
     
 }
--- src/modelo/Producto.java
+++ src/modelo/Producto.java
@@ -3,11 +3,60 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package modelo;
-
-/**
- *
- * @author MI PC
- */
 public class Producto {
+    int Id;
+    String nombre, descripcion;
+    Categoria categoria;
+    double precio;
+
+    public Producto(int Id, String nombre, String descripcion, Categoria categoria, double precio) {
+        this.Id = Id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.categoria = categoria;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId (int Id) {
+        this.Id = Id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    
     
 }
